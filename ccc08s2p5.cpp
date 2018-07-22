5720036910e39cfff2668117437f0e02c8add6c8
dequinox <umarov.doro@gmail.com>
2018-07-22 22:16:20 +0300

    Solved Problem: Candy

--- ccc08s2p5.cpp
+++ ccc08s2p5.cpp
@@ -1,53 +1,67 @@
+/*
+ *
+ * 2008 Canadian Computing Competition, Day 2
+ * Day 2, Problem 2: Candy
+ *
+*/
+
+
 #include <stdio.h>
 #include <map>
+#include <queue>
+#include <string.h>
 #include <iostream>
 
 const int MAXN = 101;
 const int MAXK = 501;
 const int INF  = 1000000000;
 
-int diff[MAXN][MAXK];
-	int amount[MAXN];
-	int calorie[MAXN];
-
-int mindifference = INF;
-int boundary = 0;
-bool visited[MAXN][MAXK * 20000];
-
-void run(int N, int difference)
-{
-	if (visited[N][std::abs(difference)]) return;
-	visited[N][std::abs(difference)] = true;
-
-	//if (difference > boundary || difference < -boundary) return;
-
-
-	if (N == 0) 
-	{
-		mindifference = std::min(mindifference, std::abs(difference));
-		return;
-	}
-	for (int i = 0; i <= amount[N]; i++)
-	{
-		run(N - 1, difference - (amount[N] - i) * calorie[N] + i * calorie[N]);
-	}
-}
+int amount[MAXN];
+int calorie[MAXN];
+bool visited[MAXK * MAXN * 200];
+int point[MAXK*MAXN*200];
+int count[MAXK*MAXN*200];
 
 int main()
 {
 	int N;
+	int total = 0;
 
 	scanf("%d", &N);
 	for (int i = 1; i <= N; i++)
 	{
-		scanf("%d%d", &amount[i], &calorie[i]);
-		boundary += amount[i] * calorie[i];
+		scanf("%d%d", &amount[i], &calorie[i]);										                
+		total += amount[i] * calorie[i];		
 	}
-	boundary /= 1.5;
 
-	run(N, 0);
+	point[++point[0]] = 0;
+	visited[0] = true;
+	count[0] = 0;
 
-	printf("%d\n", mindifference);
+	for (int i = 1; i <= N; i++)
+	{
+		memset(count, 0, total/2 * sizeof(int));
+
+		for (int j = 1; j <= point[0]; j++)
+		{
+			if (point[j] + calorie[i] <= total / 2 && !visited[point[j] + calorie[i]] && count[point[j]] + 1 <= amount[i])
+			{
+				count[point[j] + calorie[i]] = count[point[j]] + 1;
+				visited[point[j] + calorie[i]] = true;
+				point[++point[0]] = point[j] + calorie[i];
+			}
+		}
+	}
+
+	for (int i = total / 2; i >= 0; i--)
+	{
+		if (visited[i])
+		{
+			printf("%d\n", total - i * 2);
+			break;
+		}
+	}
 	return 0;
-	
+
 }
+
